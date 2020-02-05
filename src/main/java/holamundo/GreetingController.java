0e6c5a3db70abc992f462108973088af3a60597f
Celia Merino Valladolid <celia-merino1997@hotmail.com>
2020-02-05 17:51:02 +0100

    primera estructura html

--- src/main/java/holamundo/GreetingController.java
+++ src/main/java/holamundo/GreetingController.java
@@ -1,9 +1,5 @@
 package holamundo;
 
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
